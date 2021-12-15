bada1711c5c09e2340e57cfc0dfff62034656a26
srinivasaraghavan <srinivasa.raghavan@thoughtworks.com>
2021-12-15 23:03:17 +0530

    day 1 problem 1 done

---
+++ src/main/kotlin/day1/problem1.kt
@@ -0,0 +1,12 @@
+import java.io.File
+
+fun main() {
+    val list = File("src/main/resources/day1/problem1_input").readLines()
+    var answer=0
+    for(i in 1 until list.size)
+    {
+        if(list[i].toBigInteger()>list[i-1].toBigInteger())
+            ++answer
+    }
+    println(answer)
+}
