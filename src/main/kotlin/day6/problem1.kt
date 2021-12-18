1a2c0b7fdcc6b922ad657b2609c51736038b2652
srinivasaraghavan <srinivasa.raghavan@thoughtworks.com>
2021-12-18 23:48:00 +0530

    day 6 problem 2 done

--- src/main/kotlin/day6/problem1.kt
+++ src/main/kotlin/day6/problem1.kt
@@ -1,21 +1,25 @@
 package day6
 
 import java.io.File
+import java.util.concurrent.CompletableFuture
 
 fun main() {
     val input = File("src/main/resources/day6/input.txt").readText()
 
     val list = input.split(",").map { it.toInt() }.toMutableList()
 
-    for(i in 1..80){
-        for(j in list.indices){
-            if(list[j] == 0){
-                list[j] = 6
-                list.add(8)
-            }
-            else --list[j]
-        }
-    }
+    val fish = Array<Long>(10){ 0 }.toMutableList()
 
-    println(list.size)
+    list.forEach {
+        ++fish[it]
+    }
+    for(day in 1..256){
+        fish[7] += fish[0]
+        fish[9] = fish[0]
+        for(i in 0 until 9){
+            fish[i] = fish[i+1]
+        }
+        fish[9] = 0
+    }
+    println(fish.sum())
 }
